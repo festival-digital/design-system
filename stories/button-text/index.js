79b2ed83a8b2a854bf02f20f9999dcddca3fbed0
Natalia Luiza <natalialuizas@live.com>
2020-08-26 13:09:55 -0300

    adding underline button and arrow button

---
+++ stories/button-text/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import { ArrowButton, UnderlineButton } from './button-text.style';
+
+const ButtonText = ({ underline, children, small, ...props }) => {
+  if (underline) {
+    return (
+      <UnderlineButton {...props} small={small}>
+        {children}
+      </UnderlineButton>
+    );
+  }
+  return (
+    <ArrowButton {...props} small={small}>
+      <ArrowBackIosIcon
+        small={small}
+        style={{
+          fontSize: small ? '16px' : '24px',
+          verticalAlign: 'middle',
+          display: 'inline-block',
+          marginRight: '12px',
+        }}
+      />
+      {children}
+    </ArrowButton>
+  );
+};
+
+ButtonText.propTypes = {
+  underline: PropTypes.bool,
+  white: PropTypes.bool,
+  children: PropTypes.oneOfType([PropTypes.node, PropTypes.string]).isRequired,
+};
+
+ButtonText.defaultProps = {
+  underline: true,
+  white: false,
+};
+
+export default ButtonText;
