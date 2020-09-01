4301aa180b284634c98795bb9856eb28a1a781c4
Gabriel <gabrielfurlan05@gmail.com>
2020-09-01 14:04:54 -0300

    export componets on index

--- stories/index.js
+++ stories/index.js
@@ -1,8 +1,14 @@
 import './assets/reset.css';
 
 export { default as defaultTheme } from '../theme';
+
 export { default as Button } from './button';
+export { default as ButtonText } from './button-text';
+export { default as CheckboxInput } from './checkbox-input';
 export { default as IDARegisterButton } from './ida-register-button';
 export { default as IDALoginButton } from './ida-login-button';
+export { default as SmallText } from './small-text';
+export { default as Subtitle } from './subtitle';
+export { default as Text } from './text-input';
 export { default as TextInput } from './text-input';
-export { default as ButtonText } from './button-text';
+export { default as Title } from './title';
