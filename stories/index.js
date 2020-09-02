093a23545c00bf49f9ff5ce9f593fb107b833759
Natalia Luiza <natalialuizas@live.com>
2020-09-02 19:26:32 -0300

    adding the SwitchButton export to the index.js file

--- stories/index.js
+++ stories/index.js
@@ -12,3 +12,4 @@ export { default as Subtitle } from './subtitle';
 export { default as Text } from './text';
 export { default as TextInput } from './text-input';
 export { default as Title } from './title';
+export { default as SwitchButton } from './switch-button';
