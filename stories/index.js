6cafb50f3b2120fcb645f99081085fa007ab40e5
Mauricio Maletta <mauricio_maletta@hotmail.com>
2020-09-11 19:41:44 -0300

    export icon-button,

--- stories/index.js
+++ stories/index.js
@@ -5,6 +5,7 @@ export { default as defaultTheme } from '../theme';
 export { default as Button } from './button';
 export { default as ButtonText } from './button-text';
 export { default as CheckboxInput } from './checkbox-input';
+export { default as IconButton } from './icon-button';
 export { default as IDARegisterButton } from './ida-register-button';
 export { default as IDALoginButton } from './ida-login-button';
 export { default as SmallText } from './small-text';
