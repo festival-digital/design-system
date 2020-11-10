c9001e3feaf54cf947dec88531244aca9a0b0da2
Gabriel <gabrielfurlan05@gmail.com>
2020-11-10 13:12:20 -0300

    fixing images

--- stories/ida-login-button/index.js
+++ stories/ida-login-button/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { WhiteButton, DarkButton, Icon } from './IDALoginButton.style';
 
 const BASE_ICON_URI =
-  'http://ida-design-system-assets.s3-us-west-2.amazonaws.com/icons';
+  'https://ida-design-system-assets.s3-us-west-2.amazonaws.com/icons';
 const ALT_ICON_TEXT =
   'Essa é a Logo da Identidade Digital Ativista, a logo é sigla da marca onde cada letra é de uma cor especifica (a letra I é verde, a letra D é vermelha e a A é roxo) e formam formas geométricas.';
 
